60f0080c107fe6a7ef1572e71e808d0d48a80b3b
Michael Hicks <hicks.michael04@gmail.com>
2023-05-12 17:16:01 -0600

    Update routes for API v2

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ Rails.application.routes.draw do
     namespace :v1 do 
       resources :books, only: %i[index show create update destroy]
     end 
+    
+    namespace :v2 do 
+      resources :books, only: %i[index]
+    end 
   end 
 end

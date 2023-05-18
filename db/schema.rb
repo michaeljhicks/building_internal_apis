12093c2186b99a0ec8739d4eca4242531683144d
Michael Hicks <hicks.michael04@gmail.com>
2023-05-17 20:51:54 -0700

    Add Authors

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_09_150224) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_18_035037) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "authors", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "books", force: :cascade do |t|
     t.string "title"

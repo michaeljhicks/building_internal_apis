e775b2d29822b0be6a5ec74ca05b079d16f14adf
Michael Hicks <hicks.michael04@gmail.com>
2023-05-20 21:51:37 -0700

    Book model validations fully tested

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,14 +1,16 @@
 class Book < ApplicationRecord
+  validates :title, :genre, :summary, :popularity, presence: true
+  validates :number_sold, presence: true, numericality: true
 
-    before_save { |book| book.popularity = calculate_popularity }
+  before_save { |book| book.popularity = calculate_popularity }
 
-    private 
-    
-    def calculate_popularity
-      if number_sold > 5
-        'high'
-      else 
-        'low'
-      end
+  private 
+  
+  def calculate_popularity
+    if number_sold > 5
+      'high'
+    else 
+      'low'
     end
+  end
 end

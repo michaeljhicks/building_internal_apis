1e9441db48c5afaad630a3ae26481d42399df335
Michael Hicks <hicks.michael04@gmail.com>
2023-05-20 21:53:56 -0700

    Book belongs to Author

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,7 +1,12 @@
 class Book < ApplicationRecord
+  # model validations
   validates :title, :genre, :summary, :popularity, presence: true
   validates :number_sold, presence: true, numericality: true
 
+  # model relationships
+  belongs_to :author
+
+  # call_back for v2 API adding popularity to DB
   before_save { |book| book.popularity = calculate_popularity }
 
   private 

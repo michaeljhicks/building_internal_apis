673957cb531a970a18ce424c7587bb898fe530c3
Michael Hicks <hicks.michael04@gmail.com>
2023-05-24 16:56:03 -0600

    Test: Author model relationships

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -5,4 +5,8 @@ RSpec.describe Author, type: :model do
     it { should validate_presence_of :first_name }
     it { should validate_presence_of :last_name }
   end 
+
+  describe 'relationships' do 
+    it { should have_many :books }
+  end 
 end
